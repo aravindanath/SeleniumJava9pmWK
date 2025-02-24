a4de70c415952958981925ebd9ac31d723e3f3c5
vikas <vikasa.kappali@gmail.com>
2025-02-24 22:18:18 +0530

    Adding assignments

---
+++ src/test/java/assignment_vikas/Assignment2.java
@@ -0,0 +1,11 @@
+package assignment_vikas;
+
+public class Assignment2 {
+    public static void main(String[] args) {
+String name = "Java assignment";
+        int position1 = 6;
+        int position2 = 10;
+        System.out.println("The character at position "  + position1 +  " is " +  name.charAt(position1));
+        System.out.println("The character at position "  +  position2 +  " is " + name.charAt(position2));
+    }
+}
---
+++ src/test/java/assignment_vikas/Assignment3.java
@@ -0,0 +1,9 @@
+package assignment_vikas;
+
+public class Assignment3 {
+    public static void main(String[] args) {
+        String word1 = "Java and";
+        String word2 = "Selenium";
+        System.out.println(word1 + " ".concat(word2));
+    }
+}
