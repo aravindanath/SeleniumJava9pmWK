e4cca0606d8d349e45f7c3e9e267569c74f130d5
chetan_my <chetankumar.my9292@gmail.com>
2025-02-25 00:09:17 +0530

    Adding assignments

---
+++ src/test/java/Assignment_Chetan/String_Length.java
@@ -0,0 +1,11 @@
+package Assignment_Chetan;
+
+public class String_Length {
+
+    public static void main(String[] args) {
+
+        String str = "example.com";
+
+        System.out.println("The string length of '" + str + "' is: " + str.length());
+    }
+}
