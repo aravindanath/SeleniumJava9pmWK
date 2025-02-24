0aa078a7530a3718e14211e02165f689202ae6c8
chetan_my <chetankumar.my9292@gmail.com>
2025-02-25 00:13:56 +0530

    Adding assignments

---
+++ src/test/java/Assignment_Chetan/Replace_Character.java
@@ -0,0 +1,15 @@
+package Assignment_Chetan;
+
+public class Replace_Character {
+
+    public static void main(String[] args) {
+
+        String str = "The quick brown fox jumps over the lazy dog";
+
+        System.out.println("Original String: " + str);
+
+        String newStr = str.replace("d", "f");
+
+        System.out.println("New String: " + newStr);
+    }
+}
